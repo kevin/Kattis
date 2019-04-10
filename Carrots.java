2df0f6a30363b62fd0be60426837cc488c497757
Kevin Xu <35074883+kvnxu@users.noreply.github.com>
2019-04-10 18:52:56 -0500

    Create Carrots.java

---
+++ Carrots.java
@@ -0,0 +1,17 @@
+// open.kattis.com/problems/carrots
+import java.util.*;
+import java.io.*;
+
+public class Carrots {
+    
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        int dhwuhduiajknkjndjk = in.nextInt();
+        int uduhwioudujioawoaopkposmklmkdklasdbiohduiwhuidhaiuhduia = in.nextInt();
+        
+        System.out.println(uduhwioudujioawoaopkposmklmkdklasdbiohduiwhuidhaiuhduia);
+        
+    }
+    
+}
