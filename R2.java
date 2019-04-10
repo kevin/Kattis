75ae91fc54871617349b56ff67ca8bae23ba727d
Kevin Xu <35074883+kvnxu@users.noreply.github.com>
2019-04-10 18:51:41 -0500

    Create R2.java

---
+++ R2.java
@@ -0,0 +1,17 @@
+// open.kattis.com/problems/r2
+import java.util.*;
+import java.io.*;
+
+public class R2 {
+    
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        int one = in.nextInt();
+        int two = in.nextInt();
+        
+        System.out.println((two-one)+two);
+        
+    }
+    
+}
