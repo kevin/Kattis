9ff80e7db4061fc66a929be79350a8f8040cba4e
Kevin Xu <35074883+kvnxu@users.noreply.github.com>
2019-07-09 16:58:51 -0500

    Add files via upload

---
+++ EncodedMessage.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class EncodedMessage {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		int numLines = in.nextInt();
+		in.nextLine();
+
+		for (int i = 0; i < numLines; i++) {
+
+			String line = in.nextLine();
+			int x = (int) (Math.sqrt(line.length()));
+			String message = "";
+			int index = x - 1;
+			while (message.length() < line.length()) {
+
+				if (index >= line.length()) {
+
+					index = index - 1 - line.length();
+
+				}
+
+				message += line.charAt(index);
+				index += x;
+
+			}
+
+			System.out.println(message);
+
+		}
+
+	}
+
+}
